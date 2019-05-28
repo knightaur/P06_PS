8e165ad2a572425cab21dee4bdb0cb2666af98dd
knightaur <francisanthonytoha@gmail.com>
2019-05-28 13:00:57 +0800

    1st commit

---
+++ app/src/main/java/sg/edu/rp/c346/p06_ps/DBHelper.java
@@ -0,0 +1,82 @@
+package sg.edu.rp.c346.p06_ps;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+import java.util.ArrayList;
+
+public class DBHelper extends SQLiteOpenHelper {
+
+    private static final String DATABASE_NAME = "tasks.db";
+    private static final int DATABASE_VERSION = 2;
+    private static final String TABLE_TASK = "task";
+    private static final String COLUMN_ID = "_id";
+    private static final String COLUMN_NAME = "name";
+    private static final String COLUMN_DESC = "description";
+
+    public DBHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String createNoteTableSql = "CREATE TABLE " + TABLE_TASK + "("
+                + COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT,"
+                + COLUMN_NAME + " TEXT,"
+                + COLUMN_DESC + " TEXT)";
+        db.execSQL(createNoteTableSql);
+        Log.i("info", "created tables");
+
+        ContentValues values = new ContentValues();
+        values.put(COLUMN_NAME, "Buy Milk");
+        values.put(COLUMN_DESC, "Low Fat");
+        db.insert(TABLE_TASK, null, values);
+
+    }
+
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("ALTER TABLE " + TABLE_TASK + " ADD COLUMN module_name TEXT ");
+    }
+
+    public long insertNote(String name, String desc) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put(COLUMN_NAME, name);
+        values.put(COLUMN_DESC, desc);
+        long result = db.insert(TABLE_TASK, null, values);
+        db.close();
+        Log.d("SQL Insert ",""+ result); //id returned, shouldn’t be -1
+        return result;
+    }
+
+    public ArrayList<Task> getAllNotes(String keyword) {
+        ArrayList<Task> tasks = new ArrayList<Task>();
+
+        SQLiteDatabase db = this.getReadableDatabase();
+        String[] columns= {COLUMN_ID, COLUMN_NAME, COLUMN_DESC};
+
+        String condition = COLUMN_NAME + " Like ?";
+        String[] args = { "%" +  keyword + "%"};
+        Cursor cursor = db.query(TABLE_TASK, columns, condition, args,
+                null, null, null, null);
+
+        if (cursor.moveToFirst()) {
+            do {
+                int id = cursor.getInt(0);
+                String name = cursor.getString(1);
+                String desc = cursor.getString(2);
+                Task task = new Task(id, name, desc);
+                tasks.add(task);
+            } while (cursor.moveToNext());
+        }
+        cursor.close();
+        db.close();
+        return tasks;
+    }
+}
